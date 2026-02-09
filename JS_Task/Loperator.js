49c560f8910a832f0f8549c0d28eeb6d038ecce2
GayatrK-gk <gayatrik6566@gmail.com>
2026-02-10 00:15:16 +0530

    Added JavaScript tasks

---
+++ JS_Task/Loperator.js
@@ -0,0 +1,18 @@
+let age=20;
+hasId=true;
+if(age>=18 && hasId===true){
+    console.log("You can vote");
+}
+else{
+    console.log("You cant vote");
+}
+
+let isWeekend=false;
+let isHoliday=true;
+
+if(isWeekend || isHoliday){
+    console.log("Take leave");
+}
+else{
+    console.log("Go to work");
+}
