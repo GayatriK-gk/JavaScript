36c8ff39714f94ceaec91ca0323581cdd4c80364
GayatrK-gk <gayatrik6566@gmail.com>
2026-02-12 01:17:50 +0530

    Input

---
+++ JS_Task/Input.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById("btn").addEventListener("click", function () {
+    let value = document.getElementById("inputText").value;
+    document.getElementById("output").innerText = value;
+});
